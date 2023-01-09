712dbde5a6af69644fefcf9c530989a13c083b3d
Vlynn01 <vlynn00@yahoo.com>
2023-01-09 22:48:06 +0700

    DNDK

---
+++ src/main/java/com/example/case_study_be_md6/repository/before/IRoleRepo.java
@@ -0,0 +1,7 @@
+package com.example.case_study_be_md6.repository.before;
+
+import com.example.case_study_be_md6.model.before.Roles;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IRoleRepo extends CrudRepository<Roles,Long> {
+}
