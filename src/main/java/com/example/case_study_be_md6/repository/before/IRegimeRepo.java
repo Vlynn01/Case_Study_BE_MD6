809b38952dacf601eb32f088fb155e05cc11cadf
hoangquangngoc <hoangquangngoc10@gmail.com>
2023-01-09 23:08:50 +0700

    ngoc

---
+++ src/main/java/com/example/case_study_be_md6/repository/before/IRegimeRepo.java
@@ -0,0 +1,8 @@
+package com.example.case_study_be_md6.repository.before;
+
+import com.example.case_study_be_md6.model.before.Regime;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IRegimeRepo extends CrudRepository<Regime,Integer>{
+
+}
