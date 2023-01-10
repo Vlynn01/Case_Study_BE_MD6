717a0f3e78f2d2baf4423aa8694999002e8d4eb5
Vlynn01 <vlynn00@yahoo.com>
2023-01-10 08:35:30 +0700

    repo

--- src/main/java/com/example/case_study_be_md6/repository/before/IAppUserRepo.java
+++ src/main/java/com/example/case_study_be_md6/repository/before/IAppUserRepo.java
@@ -15,7 +15,7 @@ public interface IAppUserRepo extends CrudRepository<AppUser, Long> {
     // ĐỔi mật khẩu
     @Modifying
     @Transactional
-    @Query(nativeQuery = true,value = "update  case_study_be_md6.app_user set password=:password where email=:email")
+    @Query(nativeQuery = true,value = "update  case_study_md6.app_user set password=:password where email=:email")
     void changPassword(@Param("email") String gmail,@Param("password") String password);
 
 
